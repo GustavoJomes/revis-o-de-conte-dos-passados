4c018310e20713647078ee860eb0444b31bdef9b
Likael <protol.lapp@gmail.com>
2025-10-31 21:14:32 -0300

    Avalicoes criadas

---
+++ app.py
@@ -0,0 +1,19 @@
+from Modelos.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'gourmet')
+restaurante_mexicano = Restaurante('mexican food', 'comida mexicana')
+restaurante_japones = Restaurante('japa', 'japonesa')
+
+restaurante_mexicano.alternar_estado()
+
+restaurante_mexicano.receber_avaliacao('João', 5)
+restaurante_mexicano.receber_avaliacao('Pedro', 4)
+restaurante_mexicano.receber_avaliacao('Carlos', 8)
+restaurante_mexicano.receber_avaliacao('Ronaldo', 10)
+
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
